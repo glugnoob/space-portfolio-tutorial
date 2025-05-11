12d0ddcb36a47ec6bce2543e0a4efe67c04503e6
glugnoob <r.gluger23@gmail.com>
2025-05-11 17:36:33 +0100

    before-adding-ice

--- src/pages/skills-page.tsx
+++ src/pages/skills-page.tsx
@@ -2,6 +2,25 @@ import { pageVariants } from "@/lib/constants/page-transitions";
 import { motion, useScroll, useTransform } from "framer-motion";
 import SpaceBackground from "../assets/skybox/Spacebox_bottom.png";
 import { useRef } from "react";
+import { WEB_STACK } from "@/lib/constants/skills-page-constants";
+
+const STAGGER_TIME = 0.2;
+const INITIAL_DELAY = 1;
+
+const SKILLS_CONTAINER_VAR = {
+  initial: {},
+  animate: {
+    transition: {
+      delayChildren: 0.6,
+      staggerChildren: STAGGER_TIME,
+    },
+  },
+};
+
+const SKILLS_CHILD = (direction: string) => ({
+  initial: { x: direction === "left" ? "-100vw" : "100vw" },
+  animate: { x: "0px", transition: { duration: 0.3, ease: "easeOut" } },
+});
 
 const SkillsPage = () => {
   const containerRef = useRef<HTMLElement | null>(null);
@@ -27,6 +46,51 @@ const SkillsPage = () => {
       }}
     >
       <h1 className="text-5xl">Skills:</h1>
+      <motion.div
+        variants={SKILLS_CONTAINER_VAR}
+        initial="initial"
+        animate="animate"
+        className="flex flex-wrap items-center gap-10 mt-14"
+      >
+        {WEB_STACK.map((item, itemI) => (
+          <motion.div
+            variants={SKILLS_CHILD(itemI % 2 === 0 ? "left" : "right")}
+            key={`web-stack-${itemI}`}
+            className="flex flex-col items-start gap-3 text-xl font-bold capitalize text-white"
+          >
+            <div className="flex flex-col p-2 rounded-lg hover-container bg-slate-500/5">
+              <img src={item.image} className="object-contain mb-2 size-24" />
+              <div>
+                {item.name.split("").map((char, i) => (
+                  <motion.span
+                    key={`char-${char}-${i}`}
+                    initial={{ color: "#FFF" }}
+                    animate={{ color: ["#FFF", "#180716", "#FFF"] }}
+                    transition={{
+                      duration: 3,
+                      delay: STAGGER_TIME * itemI + i * 0.01 + INITIAL_DELAY,
+                    }}
+                    className="mr-1"
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </motion.div>
+      <h3 className="mt-8 font-bold text-4xl">About Me:</h3>
+      <p className="mt-4 text-2xl">
+        I'm a self taught web/app developer with a passion for creating
+        immersive apps with great UI/UX in both 3D and 2D space. I love learning
+        new emerging technologies to make development more efficient whilst
+        still maintaining a high quality product. I have a in depth
+        understanding of all the parts that make an application functional from
+        authentication, styling, responsiveness, database interaction / crud
+        operations, global state management and animation. Be sure to get in
+        touch if you would like to know more!
+      </p>
       <div style={{ paddingBottom: 3000 }} />
     </motion.section>
   );
