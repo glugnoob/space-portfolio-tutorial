9baf7bbe78fea7a3c9f34bde4d5a9915c7948a68
glugnoob <r.gluger23@gmail.com>
2025-05-13 16:40:01 +0100

    pre-lint

--- src/components/html-loader.tsx
+++ src/components/html-loader.tsx
@@ -3,7 +3,7 @@ import { Html } from "@react-three/drei";
 const HtmlLoader = () => {
   return (
     <Html className="flex items-center justify-center w-screen h-screen">
-      <div className="border-8 border-t-0 border-l-0 rounded-full border-white border-opacity-70 size-20 animate-spin" />
+      <div className="border-8 border-t-0 border-l-0 rounded-full border-white/70 size-20 animate-spin" />
     </Html>
   );
 };
